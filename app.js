b57da379fea6e6c11b18c7b3b3837147ebf6d3db
laolee <laolee010126@gmail.com>
2021-01-06 01:28:34 +0800

    feature: Make basic res template for client
    make foo.in
    execute accordfing exe file
    get foo.out
    sned it to client

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const fs = require("fs");
 const cors = require("cors");
+const exec = require("child_process").execFile;
 
 const app = express();
 
@@ -8,18 +9,44 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
+//input fileName and req.body data will make res to client
+const makeResponse = async (fileName, data) => {
+  await fs.writeFile(`${fileName}.in`, data, (err) => {
+    console.log(`${fileName}.in file is created`);
+    exec(`${fileName}.exe`, () => {
+      console.log(`${fileName}.exe is excecuted`);
+      fs.readFile(`${fileName}.out`, (err, data) => {
+        const output = data.toString();
+        res.json({ data: output });
+      });
+    });
+  });
+};
+
+// send back output
 app.post("/panduan", async (req, res) => {
   const { data } = req.body;
-  await fs.writeFile("panduan.in", data, (err) => {
-    console.log("haha");
-  });
-  fs.open("panduan.exe", () => {
-    console.log("exp file opned");
-  });
+  if (fs.existsSync("panduan.in")) {
+    //if file exist, delete first
+    fs.unlink("panduan.in", () => {
+      makeResponse("panduan", data);
+    });
+  } else {
+    makeResponse("panduan", data);
+  }
 });
 
+// send back output
 app.post("/shengfu", async (req, res) => {
-  const { query } = await req.body;
+  const { data } = req.body;
+  if (fs.existsSync("shengfu.in")) {
+    //if file exist, delete first
+    fs.unlink("shengfu.in", () => {
+      makeResponse("shengfu", data);
+    });
+  } else {
+    makeResponse("shengfu", data);
+  }
 });
 
 app.listen(3000, () => {
